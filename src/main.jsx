0d08d1cf62a44d4dd5ce71c51ed6394ff47a1bdf
Gourav Anand <gourav.anand@sonata-software.com>
2024-02-22 00:25:12 +0530

    Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,6 @@
 import React,{useState} from "react";
 import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Route, Routes,Navigate } from "react-router-dom";
-
-// import Home from "./App.jsx";
-// import Login from "./Components/Login.jsx";
-// import Register from "./Components/Register.jsx";
 import "./index.css";
-// import AdminForm from "./Components/AdminForm.jsx";
-// import Layout from "antd/es/layout/layout.js";
-// import Location from "./Components/Location.jsx";
-// import Hospital from "./Components/Hospital.jsx";
-// import Doctorlist from "./Components/Doctotlist.jsx";
-// import DoctorDetails from "./Components/DoctorDetails.jsx";
  import RoutesCond from "./RoutesCond.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
