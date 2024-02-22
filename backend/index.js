61c7f08d110c7f200982422e7badb00a26f40e4b
Junaidve3 <junaid.nayeem@ve3.global>
2024-02-22 21:43:34 +0530

    mongo uri added

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
-
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use("/", routes);
 
-
-mongoose.connect('mongodb://localhost:27017/mongo', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log('Connected to MongoDB');
+mongoose
+  .connect(
+    "mongodb+srv://junaid:XyzlTzULFmok8PoC@emedico.p1sd8el.mongodb.net/?retryWrites=true&w=majority&appName=Emedico",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
+      console.log(`Server is running on port ${PORT}`);
     });
-})
-.catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
