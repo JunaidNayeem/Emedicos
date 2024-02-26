c032838d437015cdd3c13f3b33df61cb86085181
Gourav Anand <gouravanand8799@gmail.com>
2024-02-26 15:32:23 +0530

    changes made

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,58 @@
-require("dotenv").config();
+// require("dotenv").config();
 
-const express = require("express");
-const mongoose = require("mongoose");
-const routes = require("./routes");
-const cors = require("cors");
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const routes = require("./routes");
+// const cors = require("cors");
+// const app = express();
+// const PORT = process.env.PORT || 5000;
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", routes);
+
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+
+
+
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
+
 app.use(cors());
 app.use(express.json());
-app.use("/", routes);
+app.use('/', routes);
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
+mongoose.connect('mongodb://localhost:27017/mongo', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
+})
+.then(() => {
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}`);
     });
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+})
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
--- src/Components/Location.jsx
+++ src/Components/Location.jsx
@@ -1,37 +1,27 @@
 import React, { Component } from 'react';
-import { CitySelect, CountrySelect, StateSelect, LanguageSelect } from 'react-country-state-city';
+import { CitySelect, CountrySelect, StateSelect } from 'react-country-state-city';
 import 'react-country-state-city/dist/react-country-state-city.css';
 import HeaderComponent from "./Header";
 import "../assets/sass/location.scss";
-const { Content } = Layout;
-import { Layout,Button  } from 'antd';
+import { Layout, Button } from 'antd';
 import { Link } from 'react-router-dom';
+
+const { Content } = Layout;
 
 export default class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            countryid: 0,
-            stateid: 0,
-            cityid:0,
+            countryid: 101, 
+            stateid: 4037, 
+            cityid: 0,
         };
-    }
-
-    handleCountryChange = (e) => {
-        this.setState({ countryid: e.id });
-        localStorage.setItem('selectedCountry', e.id); 
-    }
-
-    handleStateChange = (e) => {
-        this.setState({ stateid: e.id });
-        localStorage.setItem('selectedState', e.id); 
     }
 
     handleCityChange = (e) => {
         this.setState({ cityid: e.id });
-        localStorage.setItem('selectedCity', e.id); 
+        localStorage.setItem('selectedCity', e.name); 
     }
-
 
     render() {
         const { countryid, stateid, cityid } = this.state;
@@ -42,21 +32,24 @@ export default class App extends Component {
                     <div className="location">
                         <h6>Country</h6>
                         <CountrySelect
+                            value={countryid} 
+                            disabled={true} 
                             onChange={this.handleCountryChange}
-                            placeHolder="Select Country"
+                            placeHolder="INDIA"
                         />
                         <h6>State</h6>
                         <StateSelect
                             countryid={countryid}
+                            value={stateid} 
+                            disabled={true} 
                             onChange={this.handleStateChange}
-                            placeHolder="Select State"
+                            placeHolder="BIHAR"
                         />
                         <h6>City</h6>
                         <CitySelect
                             countryid={countryid}
                             stateid={stateid}
                             onChange={this.handleCityChange}
-                    
                             placeHolder="Select City"
                         />
                         <Link to="/hospital"> 
@@ -68,4 +61,3 @@ export default class App extends Component {
         );
     }
 }
-
